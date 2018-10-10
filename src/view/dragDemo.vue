2ed95b3c63fb5d2eebe5e77549d635ff5cabebc2
UF201601374\Administrator <1240277991@qq.com>
2018-10-10 16:46:52 +0800

    'init'

---
+++ src/view/dragDemo.vue
@@ -0,0 +1,49 @@
+<template>
+<div>
+	  <div class="">
+	  dragDemo页面
+	  <!-- <div @click = 'go'>点我跳转</div> -->
+	  </div>
+</div>
+
+</template>
+
+<script>
+
+
+// 传递给子组件的值
+export default{
+  data() {
+    return {
+
+    };
+  },
+  mounted () {
+    // this.$router.push({ name: 'testDemo' });
+  },
+  // created() {
+  // },
+  methods: {
+  	go() {
+  	  this.$router.push({ name: 'testDemo' });
+  	}
+  }
+  // methods: {
+  //   // 路由跳转到车辆详情页面，传入当前车的ID以及电话
+  //   toDetail(item) {
+  //     this.$router.push({ name: 'cardetail', params: { id: item.carId, mobile: item.mobile } });
+  //   },
+  //   // 方法传递
+  //   phoneCall(mobile) {
+  //     this.$emit('phoneCall', mobile);
+  //   },
+  //   getCarList_change() {
+  //     this.$emit('getCarList_change');
+  //   },
+
+  // },
+};
+</script>
+<style lang="scss">
+
+</style>
